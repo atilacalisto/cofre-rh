8da236c032967548183a78f135c14c9827d82b19
Pedro-Maciel-21 <kondfla00@gmail.com>
2026-09-11 16:21:55 -0300

    atualização  na pasta routes, na qual estava causando conflito com a main. Troquei o nome rotas_documentos.get e rotas_documentos.py para router.get e router.py e corrigiu o conflito que estava tendo na main

--- app/routes/rotas.py
+++ app/routes/rotas.py
@@ -6,19 +6,19 @@ from app.services.documento_servico import (
     criar_documento_servico,
 )
 
-roteador_documentos = APIRouter(prefix="/documentos", tags=["Documentos"])
+router = APIRouter(prefix="/documentos", tags=["Documentos"])
 
 
-@roteador_documentos.post("", response_model=Documento, status_code=201)
+@router.post("", response_model=Documento, status_code=201)
 def criar_documento(documento: Documento):
     return criar_documento_servico(documento)
 
 
-@roteador_documentos.get("", response_model=list[Documento])
+@router.get("", response_model=list[Documento])
 def listar_documentos():
     return listar_documentos_servico()
 
 
-@roteador_documentos.get("/{id}", response_model=Documento)
+@router.get("/{id}", response_model=Documento)
 def buscar_documento_por_id(id: int):
     return buscar_documento_por_id_servico(id)
